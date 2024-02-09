1b5241fb2919cbf1a2c8d06c5c816c1fbad3d7a7
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-02-09 20:11:43 +0530

    Initialising houseware-testing project

---
+++ src/todo/components/Header.jsx
@@ -0,0 +1,16 @@
+import { useCallback } from "react";
+import { Input } from "./Input";
+
+import { ADD_ITEM } from "../constants";
+
+export function Header({ dispatch, heading }) {
+    const addItem = useCallback((title) => dispatch({ type: ADD_ITEM, payload: { title } }), [dispatch]);
+
+    return (
+        <header className="header" data-testid="header">
+            <h1>{heading}</h1>
+            <h1>todos</h1>
+            <Input onSubmit={addItem} label="New Todo Input" placeholder="What needs to be done?" />
+        </header>
+    );
+}
